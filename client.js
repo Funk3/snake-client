f658efd330efe4557afd4c5bd04b6520e7a5a8f4
Zak <zak.johansson@gmail.com>
2022-11-23 15:53:48 -0800

    create function that handles user input on client side

--- client.js
+++ client.js
@@ -1,24 +1,46 @@
 const net = require("net");
+
+const handleUserInput = (key) => {
+  if (key === "\u0003") {
+    console.log("You have pressed CTRL + C");
+    process.exit()
+  }
+}
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+   // event listener
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: "50541",
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
   // return "connected!" value 
   conn.on("connect", () => console.log("You have connected successfully."))
   // send name to server 
-  conn.on("connect", () => {
-    conn.write("Name: Zak")
-  })
+  conn.on("connect", () => conn.write("Name: Zak"))
   // handle incoming data
   conn.on("data", (data) => console.log(data))
+
   return conn;
 };
 
 module.exports = {
   connect,
+  setupInput,
+  handleUserInput
 }
+
+
